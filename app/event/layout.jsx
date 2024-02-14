d0d73e57b5d1788f6efaa5b8beeb7c8a01c7f598
Cathy Wong <cathy.wong@ggmail.com>
2024-02-15 02:10:53 +0800

    event bg video

--- app/event/layout.jsx
+++ app/event/layout.jsx
@@ -2,7 +2,7 @@ export default function ShopLayout({ children }) {
   return (
     <section className="flex flex-col items-center max-h-screen absolute top-8 left-0 right-0 bottom-0">
       {children}
-      <video autoPlay muted loop className="videoTag">
+      <video autoPlay muted loop className="videoTag -z-10">
         <source src="assets/sea.mov" type="video/mp4" />
       </video>
     </section>
