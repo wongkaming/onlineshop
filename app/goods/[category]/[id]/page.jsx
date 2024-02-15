14f53737fbbf5de28dc16a43359287ce97940fb8
Cathy Wong <cathy.wong@ggmail.com>
2024-02-16 02:15:21 +0800

    fixed server side exception

--- app/goods/[category]/[id]/page.jsx
+++ app/goods/[category]/[id]/page.jsx
@@ -1,30 +1,66 @@
-import React from "react";
+"use client";
+import React, { useContext, useState, useEffect } from "react";
 import dynamic from "next/dynamic";
+import { ProductContext } from "@/context/productContext";
 import { Model3dPreview } from "@/components";
 
 const Itempage = dynamic(() => import("@/components/itempage"), {
   ssr: false,
 });
 
-async function myFunction(id) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API}/latest/clothes/item/${id}`,
-    {
-      cache: "force-cache",
+async function fetchData(id, signal) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API}/latest/clothes/item/${id}`,
+      {
+        cache: "force-cache",
+        signal: signal,
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`API call failed with status: ${response.status}`);
     }
-  );
-  if (!response.ok) {
-    throw new Error(`API call failed with status: ${response.status}`);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return null; // Or handle the error as you see fit
   }
-  return response.json();
 }
 
-export default async function Profile({ params }) {
-  const data2 = await myFunction(params.id);
+export default function Profile({ params }) {
+  const { data } = useContext(ProductContext);
+  const idToFind = params.id;
+  const index = data.findIndex((item) => item._id === idToFind);
+
+  const [data2, setData2] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetchData(params.id, signal).then((fetchedData) => {
+      setData2(fetchedData);
+    });
+
+    // Cleanup function to abort fetch on unmount
+    return () => {
+      controller.abort();
+    };
+  }, [params.id]);
+
+  if (index == -1) {
+    return (
+      <>
+        <Model3dPreview data={data2?.model3d} />
+        <Itempage data={data2} like={data2?._id} />
+      </>
+    );
+  }
+
   return (
     <>
-      <Model3dPreview data={data2?.model3d} />
-      <Itempage data={data2} like={data2?._id} />
+      <Model3dPreview data={data[index].model3d} />
+      <Itempage data={data[index]} like={data[index]._id} />
     </>
   );
 }
